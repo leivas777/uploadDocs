f29460bf449dbbfc361ffd37f55eebcf19537ed2
leivas777 <145990683+leivas777@users.noreply.github.com>
2025-08-13 14:37:26 -0300

    Ajustes na Home e no Uploads

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,8 +9,8 @@ const Home = () => {
     <div>
       <div className={styles.title}>
         <h1>Bem Vindo(a)</h1>
+        <h5>Faça upload dos seus arquivos para encaminhar para a Dra. Manuela</h5>
       </div>
-
       <div className={styles.body}>
         <div className={styles.uploads}>
           <Link to='/upload'>Fazer upload de arquivo</Link>
