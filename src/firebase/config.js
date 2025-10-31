4ce6c8202847e1170a8ef1cf2730dd75ee37973e
leivas777 <145990683+leivas777@users.noreply.github.com>
2025-10-31 11:43:12 -0300

    dotenv

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKKhM2GZLE_hf7SyXRgjDwfwVN-3psD0Y",
-  authDomain: "uploaddocslel.firebaseapp.com",
-  projectId: "uploaddocslel",
-  storageBucket: "uploaddocslel.firebasestorage.app",
-  messagingSenderId: "446639035503",
-  appId: "1:446639035503:web:738c8a61e72f0f5f28299a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
